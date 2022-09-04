f96dc1b2b28a19cbb8939de44d5c27a73bd5ac69
emasx <29961897+emasx@users.noreply.github.com>
2022-09-04 23:29:20 +0300

    objects

---
+++ script.js
@@ -0,0 +1,31 @@
+const emasArray = [
+    'De',
+    'Emas',
+    2037-1994,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+console.log(emasArray);
+
+// In objects we actually define key value pairs.
+
+const emas = {
+    firstName: 'De',
+    lastName: 'Emas',
+    age: 2022-1994,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+
+// 1. Create an object called 'myCountry' for a country of your choice, containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+    country: 'Romania',
+    capital: 'Bucharest',
+    language: 'romanian',
+    population: 19,
+    neighbours: ['Ungaria', 'Ucraina', 'Republica Moldova', 'Bulgaria', 'Serbia']
+};
